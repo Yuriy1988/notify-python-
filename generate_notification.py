852f801089a78c6d30b83419c69cf8e3af23bdd1
Daniel Omelchenko <daniel.omelchenko@digitaloutlooks.com>
2016-08-18 16:48:42 +0300

    new generation

--- generate_notification.py
+++ generate_notification.py
@@ -6,10 +6,10 @@ from pymongo import MongoClient
 motor_client = MongoClient()
 db = motor_client[config.debug.DB_NAME]
 
-
+db.notifications.remove()
 for i in range(16):
     example = {
-        "_id": uuid.uuid4(),
+        "_id": str(uuid.uuid4()),
         "name": "Template " + str(i),
         "case_regex":  "status: 401",
         "case_template": "status: {{ query.status_code }}",
